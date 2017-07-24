575a1f822c7a0e7e9ca92b139fcc737929f641ba
alun <alunfeixue@qq.com>
2017-07-24 18:14:09 +0800

    commit

--- library/src/main/java/com/daohen/netease/library/im/observe/FriendServiceManager.java
+++ library/src/main/java/com/daohen/netease/library/im/observe/FriendServiceManager.java
@@ -1,8 +1,15 @@
 package com.daohen.netease.library.im.observe;
 
+import com.daohen.netease.library.im.callback.AbstractRequestCallback;
+import com.daohen.netease.library.im.callback.NeteaseCallback;
 import com.daohen.personal.toolbox.library.Singleton;
 import com.netease.nimlib.sdk.NIMClient;
 import com.netease.nimlib.sdk.friend.FriendService;
+import com.netease.nimlib.sdk.friend.FriendServiceObserve;
+import com.netease.nimlib.sdk.friend.constant.FriendFieldEnum;
+import com.netease.nimlib.sdk.friend.model.AddFriendData;
+
+import java.util.Map;
 
 /**
  * CREATE BY DAOHEN
@@ -15,14 +22,50 @@ public class FriendServiceManager {
         return gDefault.get();
     }
 
-    public void setMessageNotify(String account, boolean notify){
-        friendService.setMessageNotify(account, notify);
+    public void addFriend(AddFriendData data, NeteaseCallback<Void> callback){
+        friendService.addFriend(data).setCallback(AbstractRequestCallback.getDefault(callback));
+    }
+
+    public void ackAddFriendRequest(String account, boolean agree, NeteaseCallback<Void> callback){
+        friendService.ackAddFriendRequest(account, agree).setCallback(AbstractRequestCallback.getDefault(callback));
+    }
+
+    public void deleteFriend(String account, NeteaseCallback<Void> callback){
+        friendService.deleteFriend(account).setCallback(AbstractRequestCallback.getDefault(callback));
+    }
+
+    public void updateFriendFields(String friendAccount, Map<FriendFieldEnum, Object> fields, NeteaseCallback<Void> callback){
+        friendService.updateFriendFields(friendAccount, fields).setCallback(AbstractRequestCallback.getDefault(callback));
+    }
+
+    public void addToBlackList(String account, NeteaseCallback<Void> callback){
+        friendService.addToBlackList(account).setCallback(AbstractRequestCallback.getDefault(callback));
+    }
+
+    public void removeFromBlackList(String account, NeteaseCallback<Void> callback){
+        friendService.removeFromBlackList(account).setCallback(AbstractRequestCallback.getDefault(callback));
+    }
+
+    public void setMessageNotify(String account, boolean notify, NeteaseCallback<Void> callback){
+        friendService.setMessageNotify(account, notify).setCallback(AbstractRequestCallback.getDefault(callback));
+    }
+
+
+
+    public FriendService getFriendService(){
+        return friendService;
+    }
+
+    public FriendServiceObserve getFriendServiceObserve(){
+        return friendServiceObserve;
     }
 
     private FriendService friendService;
+    private FriendServiceObserve friendServiceObserve;
 
     private FriendServiceManager(){
         friendService = NIMClient.getService(FriendService.class);
+        friendServiceObserve = NIMClient.getService(FriendServiceObserve.class);
     }
 
     private static final Singleton<FriendServiceManager> gDefault = new Singleton<FriendServiceManager>() {
